730de5d5c117e34d7c4ce1e8134f458ff5d79f5a
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-11-29 08:30:47 +0200

    allow worklist to be generated per machine

--- bcpp_follow/management/commands/populate_worklist.py
+++ bcpp_follow/management/commands/populate_worklist.py
@@ -1,4 +1,5 @@
 from django.apps import apps as django_apps
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from bcpp_follow.models import WorkList
@@ -25,21 +26,35 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         map_area = options['map_area']
-        subject_consents = django_apps.get_model(
-            'bcpp_subject.subjectconsent').objects.filter(
-                household_member__household_structure__household__plot__map_area=map_area)
+        try:
+            inner_container = django_apps.get_model(
+                'edc_map.innercontainer').objects.get(
+                    map_area=map_area,
+                    device_id=settings.DEVICE_ID)
+        except django_apps.get_model('edc_map.innercontainer').DoesNotExist:
+            subject_visits = django_apps.get_model(
+                'bcpp_subject.subjectvisit').objects.filter(
+                    household_member__household_structure__household__plot__map_area=map_area,
+                    visit_code__in=['T0', 'T1'])
+        else:
+            subject_visits = django_apps.get_model(
+                'bcpp_subject.subjectvisit').objects.filter(
+                    household_member__household_structure__household__plot__map_area=map_area,
+                    visit_code__in=['T0', 'T1'],
+                    household_member__household_structure__household__plot__plot_identifier__in=inner_container.identifier_labels)
         count = 0
-        total_consents = subject_consents.count()
-        for subject_consent in subject_consents:
+        total_participants = len(
+            [visit.subject_identifier for visit in subject_visits])
+        for subject_visit in subject_visits:
             try:
                 WorkList.objects.get(
-                    subject_identifier=subject_consent.subject_identifier)
+                    subject_identifier=subject_visit.subject_identifier)
             except WorkList.DoesNotExist:
                 WorkList.objects.create(
-                    subject_identifier=subject_consent.subject_identifier,
-                    map_area=subject_consent.household_member.household_structure.household.plot.map_area)
+                    subject_identifier=subject_visit.subject_identifier,
+                    map_area=subject_visit.household_member.household_structure.household.plot.map_area)
                 count += 1
                 self.stdout.write(
-                    self.style.SUCCESS(f'{count} of {total_consents} work list populated.'))
+                    self.style.SUCCESS(f'{count} of {total_participants} work list populated.'))
         self.stdout.write(
             self.style.SUCCESS('Successfully populated the work list.'))
